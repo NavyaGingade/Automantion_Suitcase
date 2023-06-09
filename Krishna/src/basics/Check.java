a3e6a500a1da3c8854433ecef3a856d05e9bb2a6
Hp <Hp@LAPTOP-5QVNBP5L>
2023-06-09 12:50:14 +0530

    FIRST TRY

---
+++ Krishna/src/basics/Check.java
@@ -0,0 +1,11 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Check {
+public static void main(String[] args) {
+	
+	WebDriver driver = new ChromeDriver();
+}
+}
